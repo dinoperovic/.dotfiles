a444fd38c678eb9d7cda7484a85fe76af950b489
Dino Perovic <dino.perovic@gmail.com>
2023-03-08 14:36:10 +0100

    up

--- lvim/link.sh
+++ lvim/link.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo "--- linking..."
-mkdir -p ~/.config/lvim
+mkdir -p ~/.config/lvim/lua
+ln -sf "`pwd`/lua/user" ~/.config/lvim/lua/user
 ln -sf "`pwd`/config.lua" ~/.config/lvim/config.lua
