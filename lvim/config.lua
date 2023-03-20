bca86e6af9e222b854555d3b4e4637575c01b214
Dino Perovic <dino.perovic@gmail.com>
2023-03-20 08:29:06 +0100

    up

--- lvim/config.lua
+++ lvim/config.lua
@@ -123,7 +123,7 @@ lvim.builtin.which_key.mappings["S"] = {
   name = "Session",
   s = { "<cmd>lua require('persistence').load()<cr>", "Restore last session for current dir" },
   l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Restore last session" },
-  Q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
+  q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
 }
 
 -- Trouble
