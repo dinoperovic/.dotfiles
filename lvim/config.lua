0eaedf60f32c33ffb438b01c3db05cf1f11c1398
Dino Perovic <dino.perovic@gmail.com>
2023-03-28 14:38:45 +0200

    add eslint

--- lvim/config.lua
+++ lvim/config.lua
@@ -44,7 +44,9 @@ formatters.setup {
   { command = "prettierd" },
 }
 local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {}
+linters.setup {
+  { command = "eslint_d" }, 
+}
 
 
 -- Plugins
