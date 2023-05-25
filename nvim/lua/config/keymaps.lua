384fa8f15d70324761c5472cd1a2b0dc22996c75
Dino Perovic <dino.perovic@gmail.com>
2023-05-25 16:27:25 +0200

    update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Buffers
+map("n", "X", function()
+	require("mini.bufremove").delete(0, false)
+end, { desc = "Delete Buffer" })
+map("n", "<leader>bl", "<cmd>BufferLineCloseRight<CR>", { desc = "Delete buffers to the right" })
+map("n", "<leader>bj", "<cmd>BufferLineCloseLeft<CR>", { desc = "Delete buffers to the left" })
