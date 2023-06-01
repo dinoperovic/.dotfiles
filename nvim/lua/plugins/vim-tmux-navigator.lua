f6a19a8230f0d53b40a0e0b86b4cb34f966d9ef7
Dino Perovic <dino.perovic@gmail.com>
2023-06-01 14:00:39 +0200

    update

---
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"christoomey/vim-tmux-navigator",
+		keys = {
+			{ "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Navigate to left window" },
+			{ "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Navigate to down window" },
+			{ "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Navigate to up window" },
+			{ "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Navigate to right window" },
+		},
+	},
+}
