8e0df9e5a9f2e67c5795ed120e6ac51f88d1bc28
Dino Perovic <dino.perovic@gmail.com>
2023-07-10 16:26:26 +0200

    update

--- nvim/lua/plugins/lang-svelte.lua
+++ nvim/lua/plugins/lang-svelte.lua
@@ -1,8 +1,18 @@
 return {
 	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"svelte",
+			})
+		end,
+	},
+	{
 		"williamboman/mason.nvim",
 		opts = function(_, opts)
-			table.insert(opts.ensure_installed, "prettierd")
+			vim.list_extend(opts.ensure_installed, {
+				"svelte-language-server",
+			})
 		end,
 	},
 	{
