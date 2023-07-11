8da85f19d3a24ace72856b6419c6a0c18602532f
Dino Perovic <dino.perovic@gmail.com>
2023-07-11 11:29:41 +0200

    update

--- nvim/lua/plugins/lang-svelte.lua
+++ nvim/lua/plugins/lang-svelte.lua
@@ -15,16 +15,4 @@ return {
 			})
 		end,
 	},
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		opts = function(_, opts)
-			local nls = require("null-ls")
-			table.insert(
-				opts.sources,
-				nls.builtins.formatting.prettierd.with({
-					extra_filetypes = { "svelte" },
-				})
-			)
-		end,
-	},
 }
