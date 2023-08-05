b2443998c68883dcfd50c9c634783219cd742c49
Dino Perovic <dino.perovic@gmail.com>
2023-08-05 21:59:15 +0200

    rm rosepine

--- fish/link.sh
+++ fish/link.sh
@@ -4,4 +4,3 @@ mkdir -p ~/.config/fish
 ln -sf "`pwd`/config.fish" ~/.config/fish/config.fish
 ln -sf "`pwd`/fishfile" ~/.config/fish/fishfile
 ln -sf "`pwd`/conf.d/_brew.fish" ~/.config/fish/conf.d/_brew.fish
-ln -sf "`pwd`/conf.d/rose-pine.fish" ~/.config/fish/conf.d/rose-pine.fish
