fdeba6bde1fb02a44974a8bc8aaa41e676be41ca
Dino Perovic <dino.perovic@gmail.com>
2023-08-06 21:19:28 +0200

    update

--- fish/link.sh
+++ fish/link.sh
@@ -4,3 +4,4 @@ mkdir -p ~/.config/fish
 ln -sf "`pwd`/config.fish" ~/.config/fish/config.fish
 ln -sf "`pwd`/fishfile" ~/.config/fish/fishfile
 ln -sf "`pwd`/conf.d/_brew.fish" ~/.config/fish/conf.d/_brew.fish
+ln -sf "`pwd`/conf.d/.gitnow" ~/.gitnow
