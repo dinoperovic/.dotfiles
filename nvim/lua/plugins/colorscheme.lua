3760689e3cfbf2c479cb09280ab0e26bd0e5724d
Dino Perovic <dino.perovic@gmail.com>
2023-08-30 15:17:35 +0200

    add gruvbox

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -6,12 +6,13 @@ return {
 	{ "sainnhe/gruvbox-material" },
 	{ "sainnhe/edge" },
 	{ "sainnhe/sonokai" },
+	{ "ellisonleao/gruvbox.nvim" },
 	{ "EdenEast/nightfox.nvim" },
 	{ "AlexvZyl/nordic.nvim", lazy = false, priority = 1000 },
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "nordic",
+			colorscheme = "gruvbox-material",
 		},
 	},
 }
