a0208408856f38466dc1acb51e03c4e1402ab35e
Dino Perovic <dino.perovic@gmail.com>
2023-08-30 15:18:25 +0200

    update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,12 +3,11 @@ return {
 	{ "rose-pine/neovim" },
 	{ "rebelot/kanagawa.nvim" },
 	{ "sainnhe/everforest" },
-	{ "sainnhe/gruvbox-material" },
+	{ "sainnhe/gruvbox-material", lazy = false, priority = 1000 },
 	{ "sainnhe/edge" },
 	{ "sainnhe/sonokai" },
-	{ "ellisonleao/gruvbox.nvim" },
 	{ "EdenEast/nightfox.nvim" },
-	{ "AlexvZyl/nordic.nvim", lazy = false, priority = 1000 },
+	{ "AlexvZyl/nordic.nvim" },
 	{
 		"LazyVim/LazyVim",
 		opts = {
