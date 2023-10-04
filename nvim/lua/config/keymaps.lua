ad26c2cf1d42b26c33764e8845280282baf9ac27
Dino Perovic <dino.perovic@gmail.com>
2023-10-04 09:05:33 +0200

    update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,11 +4,13 @@
 
 local map = vim.keymap.set
 
--- Navigation
+-- Navigation and selection
 map("i", "<C-e>", "<C-o>A", { desc = "Go to end of line" })
 map("i", "<C-a>", "<C-o>I", { desc = "Go to begining of line" })
 map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up centered" })
 map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down centered" })
+map("n", "<M-h>", "^", { desc = "Go to start of the line" })
+map("n", "<M-l>", "$", { desc = "Go to end of the line" })
 map("n", "<M-a>", "ggVG", { desc = "Select all" })
 
 -- Buffers
