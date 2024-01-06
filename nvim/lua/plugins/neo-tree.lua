e0084406668b23add55671eb8e5ed164851ddb01
Dino Perovic <dino.perovic@gmail.com>
2024-01-06 23:48:14 +0100

    update

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,11 +1,25 @@
+local Util = require("lazyvim.util")
+
 return {
 	{
 		"nvim-neo-tree/neo-tree.nvim",
+		keys = {
+			{
+				"<leader>fe",
+				function()
+					require("neo-tree.command").execute({ toggle = true, dir = Util.root(), reveal_force_cwd = true })
+				end,
+				desc = "Explorer NeoTree (root dir)",
+			},
+		},
 		opts = function(_, opts)
 			opts.window.mappings["l"] = "open"
 			opts.window.mappings["h"] = "close_node"
 			opts.filesystem.filtered_items = {
 				visible = true,
+			}
+			opts.filesystem.follow_current_file = {
+				enabled = false,
 			}
 		end,
 	},
