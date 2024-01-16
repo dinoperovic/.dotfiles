ff65538036846861fbf7cc6f7d68c1d8762577ef
Dino Perovic <dino.perovic@gmail.com>
2024-01-16 21:55:47 +0100

    add term

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	cmd = "ToggleTerm",
+	opts = {},
+	keys = {
+		{ "<C-\\>", "<cmd>ToggleTerm<cr>", mode = { "n", "i", "t" } },
+		{ "<C-h>", "<cmd>wincmd h<cr>", mode = "t" },
+		{ "<C-j>", "<cmd>wincmd j<cr>", mode = "t" },
+		{ "<C-k>", "<cmd>wincmd k<cr>", mode = "t" },
+		{ "<C-l>", "<cmd>wincmd l<cr>", mode = "t" },
+	},
+}
