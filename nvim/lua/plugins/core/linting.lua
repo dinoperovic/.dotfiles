d020944b15f2a3b231c44bb7102f4795109180eb
Dino Perovic <dino.perovic@gmail.com>
2024-01-24 19:42:34 +0100

    Update linting.lua

--- nvim/lua/plugins/core/linting.lua
+++ nvim/lua/plugins/core/linting.lua
@@ -5,13 +5,13 @@ return {
 			"williamboman/mason.nvim",
 		},
 		event = { "BufReadPre", "BufNewFile" },
-		opts = {
-			linters_by_ft = {},
-		},
 		-- stylua: ignore
 		keys = {
 			{ "<leader>cl", function() require("lint").try_lint() end, desc = "Lint Buffer" },
 		},
+		opts = {
+			linters_by_ft = {},
+		},		
 		config = function(_, opts)
 			local lint = require("lint")
 			lint.linters_by_ft = opts.linters_by_ft
