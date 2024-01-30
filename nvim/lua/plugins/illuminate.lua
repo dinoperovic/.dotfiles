bf575a206f54a833308c4e04b8f996f37ec7f4ef
Dino Perovic <dino.perovic@gmail.com>
2024-01-30 10:43:43 +0100

    update

--- nvim/lua/plugins/illuminate.lua
+++ nvim/lua/plugins/illuminate.lua
@@ -1,8 +1,12 @@
-return 	{
+return {
 	"RRethy/vim-illuminate",
 	opts = {},
 	config = function(_, opts)
 		require("illuminate").configure(opts)
+
+		-- change the highlight style
+		-- vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+		-- vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
+		-- vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
 	end,
 }
-
