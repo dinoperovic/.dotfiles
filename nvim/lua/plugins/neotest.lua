71b979512631fd7ce331c84115c82e1ce2856ce0
Dino Perovic <dino.perovic@gmail.com>
2024-03-15 09:54:18 +0100

    update

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -16,7 +16,7 @@ return {
 		{ "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest" },
 		{ "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last" },
 		{ "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
-		{ "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output" },
+		{ "<leader>to", function() require("neotest").output.open() end, desc = "Show Output" },
 		{ "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel" },
 		{ "<leader>tS", function() require("neotest").run.stop() end, desc = "Stop" },
 	},
