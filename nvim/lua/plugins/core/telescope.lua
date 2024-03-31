c4e8283f8c13892a5cdef2ac0d3b950c5f734811
Dino Perovic <dino.perovic@gmail.com>
2024-03-31 22:52:09 +0200

    update

--- nvim/lua/plugins/core/telescope.lua
+++ nvim/lua/plugins/core/telescope.lua
@@ -32,10 +32,21 @@ return {
 		pcall(require("telescope").load_extension, "fzf")
 		pcall(require("telescope").load_extension, "ui-select")
 
-		-- stylua: ignore start
-		vim.keymap.set("n", "<leader><space>", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", { desc = "Switch Buffer" })
+		vim.keymap.set(
+			"n",
+			"<leader><space>",
+			"<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",
+			{ desc = "Switch Buffer" }
+		)
 		vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<cr>", { desc = "Find Files" })
 		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>", { desc = "Find Files (Hidden)" })
+		vim.keymap.set(
+			"n",
+			"<leader>fF",
+			"<cmd>Telescope find_files hidden=true no_ignore=true<cr>",
+			{ desc = "Find Files (Hidden/Ignored)" }
+		)
+
 		vim.keymap.set("n", "<leader>fr", require("telescope.builtin").oldfiles, { desc = "Find recent Files" })
 		vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "Find recent Files" })
 		vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, { desc = "Find Buffers" })
@@ -44,7 +55,15 @@ return {
 		vim.keymap.set("n", "<leader>ss", require("telescope.builtin").builtin, { desc = "Search Select Telescope" })
 		vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "Search Help" })
 		vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "Search current Word" })
-		vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "Search by Grep" })
+
+		vim.keymap.set("n", "<leader>sg", function()
+			require("telescope.builtin").live_grep({ additional_args = { "--hidden" } })
+		end, { desc = "Search by Grep (Hidden)" })
+
+		vim.keymap.set("n", "<leader>sG", function()
+			require("telescope.builtin").live_grep({ additional_args = { "--hidden", "--no-ignore" } })
+		end, { desc = "Search by Grep (Hidden/Ignored)" })
+
 		vim.keymap.set("n", "<leader>/", require("telescope.builtin").live_grep, { desc = "Search by Grep" })
 		vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "Search Diagnostics" })
 
