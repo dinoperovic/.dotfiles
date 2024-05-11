49bc3c5c8f973651904e04d1a1e08cd0b773570c
Dino Perovic <dino.perovic@gmail.com>
2024-05-11 22:02:48 +0200

    update

---
+++ scripts/install_linux.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+echo "### updating apt..."
+sudo apt update
+
+echo "### installing base packages..."
+sudo apt install -y bat fzf ripgrep httpie wget
+mkdir -p ~/.config
+
+echo "### installing fish..."
+sudo apt install -y fish
+fish
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+rm -rf ~/.config/fish
+mkdir -p ~/.config/fish/conf.d
+
+echo "### installing lazygit..."
+sudo apt install -y install jesseduffield/lazygit/lazygit
+
+echot "### installing gpg"
+sudo apt install -y install gpg
+rm -rf ~/.gnupg
+mkdir ~/.gnupg
+chmod 700 ~/.gnupg
+
+echo "### installing neovim..."
+sudo apt install -y install neovim
+mkdir -p ~/.config/nvim
+
+echo "### installing starship..."
+sudo apt install -y install starship
+
+echo "### installing tmux..."
+sudo apt install -y install tmux
+[ ! -d "$HOME/.tmux/plugins/tpm" ] && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
