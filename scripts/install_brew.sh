bebc99642cd5847a4183e708a4873f6a2fc7c6c9
Dino Perovic <dino.perovic@gmail.com>
2024-05-16 15:46:01 +0200

    up

--- scripts/install_brew.sh
+++ scripts/install_brew.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "### installing homebrew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "yes | $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 test -d /opt/homebrew/ && eval $(/opt/homebrew/bin/brew shellenv)
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 brew update
