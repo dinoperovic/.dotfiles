08979900fd56fe771f21e873e32cd2205c93f329
Dino Perovic <dino.perovic@gmail.com>
2024-08-07 08:16:40 +0200

    update

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,40 +1,31 @@
 return {
 	"folke/which-key.nvim",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
+	dependencies = { "echasnovski/mini.icons" },
+	event = "VeryLazy",
 	opts = {
-		plugins = {
-			spelling = true,
-		},
 		icons = {
-			breadcrumb = ">>",
-			separator = "->",
+			mappings = false,
+		},
+		spec = {
+			mode = { "n", "v" },
+			{ "<leader>b", group = "buffer" },
+			{ "<leader>c", group = "code" },
+			{ "<leader>cp", group = "package" },
+			{ "<leader>f", group = "find" },
+			{ "<leader>g", group = "git" },
+			{ "<leader>gh", group = "github" },
+			{ "<leader>ghy", group = "yank" },
+			{ "<leader>h", group = "hunk" },
+			{ "<leader>ht", group = "toggle" },
+			{ "<leader>q", group = "quit/session" },
+			{ "<leader>r", group = "replace" },
+			{ "<leader>s", group = "search" },
+			{ "<leader>t", group = "test" },
+			{ "<leader>w", group = "windows" },
+			{ "<leader>x", group = "diagnostics/quickfix" },
+			{ "[", group = "prev" },
+			{ "]", group = "next" },
+			{ "g", group = "goto" },
 		},
 	},
-	config = function(_, opts)
-		require("which-key").setup(opts)
-		require("which-key").register({
-			mode = { "n", "v" },
-			["g"] = { name = "goto" },
-			["]"] = { name = "next" },
-			["["] = { name = "prev" },
-			["<leader>b"] = { name = "buffer" },
-			["<leader>c"] = { name = "code" },
-			["<leader>cp"] = { name = "package" },
-			["<leader>f"] = { name = "find" },
-			["<leader>g"] = { name = "git" },
-			["<leader>gh"] = { name = "github" },
-			["<leader>ghy"] = { name = "yank" },
-			["<leader>h"] = { name = "hunk" },
-			["<leader>ht"] = { name = "toggle" },
-			["<leader>q"] = { name = "quit/session" },
-			["<leader>r"] = { name = "replace" },
-			["<leader>s"] = { name = "search" },
-			["<leader>t"] = { name = "test" },
-			["<leader>w"] = { name = "windows" },
-			["<leader>x"] = { name = "diagnostics/quickfix" },
-		})
-	end,
 }
