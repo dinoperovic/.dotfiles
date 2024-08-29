40fb75041598c8511d114dcbc283e4b0b5084cc6
Dino Perovic <dino.perovic@gmail.com>
2024-08-29 09:35:57 +0200

    update

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,9 +1,19 @@
 -- Highlight on yank
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = highlight_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+-- Set spell for filetypes
+local spellcheck_group = vim.api.nvim_create_augroup("Spellcheck", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+	group = spellcheck_group,
+	pattern = { "markdown" },
+	callback = function()
+		vim.opt_local.spell = true
+	end,
 })
