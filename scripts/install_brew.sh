2e6520f419966bfef0d22bd71176815d55897432
Dino Perovic <dino.perovic@gmail.com>
2024-10-10 07:33:09 +0200

    update

--- scripts/install_brew.sh
+++ scripts/install_brew.sh
@@ -1,44 +1,27 @@
 #!/bin/sh
 
-echo "### installing homebrew..."
+echo "################################################################################"
+echo "### Installing homebrew..."
+echo "################################################################################"
+
 yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 test -d /opt/homebrew/ && eval "$(/opt/homebrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew update
 
 echo "### installing common packages..."
-brew install bat fzf ripgrep httpie wget stow keychain zoxide eza git gh
+brew install bat fzf ripgrep httpie wget stow keychain zoxide git gh
 mkdir -p ~/.config
-
-echo "### installing fish..."
-brew list fish || brew install fish
-
-rm -rf ~/.config/fish
-mkdir -p ~/.config/fish/conf.d
-
-echo "### installing antigen..."
-brew install antigen
-
-echo "### installing lazygit..."
-brew list lazygit || brew install jesseduffield/lazygit/lazygit
-
-echo "### installing lazydocker..."
-brew list lazydocker || brew install jesseduffield/lazydocker/lazydocker
 
 echot "### installing gpg"
 brew list gpg || brew install gpg
 mkdir -p ~/.gnupg
 chmod 700 ~/.gnupg
 
-echo "### installing neovim..."
-brew list neovim || brew install neovim
-mkdir -p ~/.config/nvim
-
-echo "### installing pyenv..."
-brew list pyenv || brew install pyenv
-
-echo "### installing go..."
-brew list go || brew install go
+echo "### installing fish..."
+brew list fish || brew install fish
+rm -rf ~/.config/fish
+mkdir -p ~/.config/fish/conf.d
 
 echo "### installing starship..."
 brew list starship || brew install starship
@@ -47,13 +30,42 @@ echo "### installing tmux..."
 brew list tmux || brew install tmux
 [ ! -d "$HOME/.tmux/plugins/tpm" ] && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-echo "### installing gcloud..."
-brew list google-cloud-sdk || brew install --cask google-cloud-sdk
+echo "### installing antigen..."
+brew install antigen
 
-echo "### installing zed..."
-brew list zed || brew install --cask zed
-rm -rf ~/.config/zed
-mkdir ~/.config/zed
+echo "################################################################################"
+echo "### Installing terminal apps..."
+echo "################################################################################"
+
+echo "### installing lazygit..."
+brew list lazygit || brew install jesseduffield/lazygit/lazygit
+
+echo "### installing lazydocker..."
+brew list lazydocker || brew install jesseduffield/lazydocker/lazydocker
+
+echo "### installing neovim..."
+brew list neovim || brew install neovim
+mkdir -p ~/.config/nvim
+
+echo "################################################################################"
+echo "### Installing languages..."
+echo "################################################################################"
+
+echo "### installing pyenv..."
+brew list pyenv || brew install pyenv
+
+echo "### installing deno..."
+brew list deno || brew install deno
+
+echo "### installing go..."
+brew list go || brew install go
+
+echo "################################################################################"
+echo "### Installing casks..."
+echo "################################################################################"
 
 echo "### installing kitty..."
 brew list kitty || brew install --cask kitty
+
+echo "### installing gcloud..."
+brew list google-cloud-sdk || brew install --cask google-cloud-sdk
